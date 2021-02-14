f90dda2182e9a6053036b7b43378741fbcb839a4
Elir-mahad <mmahad280@gmail.com>
2021-02-14 11:43:26 -0500

    added the react native navigation depenencies and imports and explanation in the notes.doc

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
+// foundational imports that come with the app
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
+// below imports are from react navigation
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+
 export default function App() {
 	return (
-		<View style={styles.container}>
-			{/* A view is the same as a div */}
-			<Text> Lets build signal !</Text>
-			<StatusBar style="auto" />
-		</View>
+		<NavigationContainer>
+			<View style={styles.container}>
+				{/* A view is the same as a div */}
+				<Text> Lets build signal !</Text>
+				<StatusBar style="auto" />
+			</View>
+		</NavigationContainer>
 	);
 }
 
@@ -25,3 +32,10 @@ const styles = StyleSheet.create({
 		// instead of justify-content its camel cast justifyContent
 	}
 });
+
+// On the web, we user react router to go between pages.
+// On react native we use react native navigation.
+// In mobile, you have different screens(pages)
+// that are stacked/layered on top of each other,
+// so for example: when you swipe back it shows the screen beneath,
+// and when you swiple forward it goes to the next screen on top.
