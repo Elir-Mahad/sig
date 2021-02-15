31722271258a7c339872dbc24754848ef126914a
Elir-mahad <mmahad280@gmail.com>
2021-02-14 21:05:27 -0500

    added the signal logo and input filed on the LoginScreen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,9 +1,11 @@
-// below are foundational imports that come with the app
+// Below are foundational imports that come with the app
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
 
-// below is the import for reacte native elements
+// Below is the import for reacte native elements -
+// it's similar to material ui in the sense that it imports
+// pre-styled elements
 import { Button, Input, Image } from "react-native-elements";
 
 const LoginScreen = () => {
@@ -12,15 +14,22 @@ const LoginScreen = () => {
 			<StatusBar style="light" />
 			{/* The status bar is the top row that has the time, battery, wifi logo etc */}
 			<Image
-				//add the image
+				//Image is a react-native-element.
+				// Here we declare that we are importing an image.
 				source={{
 					uri:
 						"https://blog.mozilla.org/internetcitizen/files/2018/08/signal-logo.png"
 				}}
-				//  from this url
+				//  The image will come from this url.
 				style={{ width: 200, height: 200 }}
-				// the image will have a width and height of 200px
+				// The image will have a width and height of 200px.
 			/>
+			<View style={styles.inputContainer}>
+				{/* This view will have a style called inputContainer */}
+				<Input placeholder="Email" />
+				{/* Input is a react native element. 
+					The Input field will have the placeholder text Email */}
+			</View>
 		</View>
 	);
 };
